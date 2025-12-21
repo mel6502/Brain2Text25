4af14db265c1b1e0361dd9c18e96e5456c92aa18
rubnch <ruben.christon@gmail.com>
2025-12-21 22:39:47 +0800

    ok

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import h5py
 from tqdm import tqdm
+import pandas as pd
+from datasets import Dataset, concatenate_datasets, load_from_disk, load_from_disk
 
 LOGIT_TO_PHONEME = [
 'BLANK',    # "BLANK" = CTC blank symbol
@@ -17,6 +19,67 @@ LOGIT_TO_PHONEME = [
 
 PHONEME_TO_LOGIT = {p: i for i, p in enumerate(LOGIT_TO_PHONEME)}
 
+def save_dataset_split_batched(split_name, save_path=None, data_path=None, session_ids=None):
+    data_path = data_path or 'data/t15_copyTask_neuralData/hdf5_data_final'
+    save_path = save_path or f'data/{split_name}_dataset'
+
+    datasets_accum = []
+
+    sessions = sorted(os.listdir(data_path))
+
+    if session_ids is not None:
+        sessions = [sessions[i] for i in session_ids]
+    print(f"Processing {len(sessions)} sessions for split '{split_name}'")
+
+    for session in tqdm(sessions):
+        session_data = {}
+        files = os.listdir(os.path.join(data_path, session))
+
+        for file in files:
+            subset = file.split('_')[-1].split('.')[0]
+            if subset != split_name:
+                continue
+
+            file_data = load_h5py_file(os.path.join(data_path, session, file))
+            for k, v in file_data.items():
+                session_data.setdefault(k, []).extend(v)
+
+        if not session_data:
+            continue
+
+        df = pd.DataFrame(session_data)
+        df["neural_features"] = df["neural_features"].apply(lambda x: x.tolist())
+
+        datasets_accum.append(Dataset.from_pandas(df, preserve_index=False))
+
+        del df, session_data
+
+    dataset = concatenate_datasets(datasets_accum)
+    dataset.save_to_disk(save_path)
+    return dataset
+
+def save_large_dataset_split_batched(split_name, save_path=None, data_path=None, session_ids=None, batch_size=10):
+    data_path = data_path or 'data/t15_copyTask_neuralData/hdf5_data_final'
+    save_path = save_path or f'data/{split_name}_dataset'
+    save_dir = os.path.dirname(save_path)
+
+    sessions = sorted(os.listdir(data_path))
+
+    for i in range(len(sessions)/batch_size):
+        session_ids = range(i*batch_size, min((i+1)*batch_size, len(sessions)))
+        batch_save_path = f"{save_path}_part{i}"
+        save_dataset_split_batched(split_name, save_path=batch_save_path, data_path=data_path, session_ids=session_ids)
+
+    datasets_accum = []
+    
+    for files in os.listdir(save_dir):
+        if files.startswith(f"{save_path}_part"):
+            dataset_part = load_from_disk(os.path.join(save_dir, files))
+            datasets_accum.append(dataset_part)
+    
+    dataset = concatenate_datasets(datasets_accum)
+    dataset.save_to_disk(save_path)
+    
 
 def load_h5py_file(file_path):
     data = {
